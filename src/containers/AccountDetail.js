ce1d2324b3ba6975892caa335579f18c53c23842
Joseph Gordy <jgordy2424@gmail.com>
2017-09-28 12:27:33 -0400

    added Withdraw funds and back to account detail buttons

--- src/containers/AccountDetail.js
+++ src/containers/AccountDetail.js
@@ -7,21 +7,18 @@ import {selectAccount} from '../actions/index';
 class AccountDetail extends Component {
 
   render() {
-    console.log("this.props: ",this.props);
     return (
       <div className="AccountDetail col-md-6">
         <div className= "card">
           <div className= "card-block">
             <h4 className= "card-title">Account Information</h4>
-            <h6 className= "card-subtitle mb-2 text-muted">{this.props.user.name}</h6>
+            <h6 className= "card-subtitle mb-2 text-muted">{this.props.account.accountType} for {this.props.user.name}</h6>
             <div className= "card-text">
-              <div>{this.props.user.email}</div>
-              <div>{this.props.user.phone}</div>
-              <div>{this.props.user.address}</div>
-
+              <div>Balance: {this.props.account.balance}</div>
             </div>
           </div>
-          <Link className="btn btn-primary" to="/users" >Back to List of Users</Link>
+          <Link className="btn btn-danger" to="/">Withdraw Funds</Link>
+          <Link className="btn btn-primary" to="/users/:id">Back to User Details</Link>
         </div>
       </div>
     );
@@ -32,7 +29,6 @@ class AccountDetail extends Component {
 // the state should reflect the reducers selectedUser and selectedAccount.
 
 function mapStateToProps(state) {
-  console.log(" AccountDetail THHAAA STATE: ", state);
   const userIdx = state.users.findIndex(user => user._id === state.selectedUser);
   const accountIdx = state.users[userIdx].accounts.findIndex(account => account.id === state.selectedAccount.id);
   return {
